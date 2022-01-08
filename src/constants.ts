e0213b2282f847bc834d76dddcf4f1444f27b637
Samuel Moreira <samuel@jssolution.com.br>
2022-01-08 00:55:37 -0300

    dotenv

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const RolesCodes = {
     DIRECTOR: 1,
     PRODUCER: 2,
     ACTOR_ACTRESS: 3,
 }
-export const salt_key = process.env.SALT_KEY;
+export const salt_key = process.env.SALT_KEY??"AAA";
